fdaea6781627601ebf14bf77c81f523f625fc813
Evgeniy Bukharev <ebukharev_1@edu.hse.ru>
2023-11-17 21:25:24 +0300

    GG_1

--- ArchitecturesShowcase/CleanSwift/Assembly.swift
+++ ArchitecturesShowcase/CleanSwift/Assembly.swift
@@ -5,4 +5,18 @@
 //  Created by Grigory Sosnovskiy on 17.11.2023.
 //
 
-import Foundation
+import UIKit
+
+enum Assembly {
+    static func build() -> UIViewController {
+        let presenter = Presenter()
+        let router = Router()
+        let interactor = Interactor(presenter: presenter)
+        let vc = CSViewController(interactor: interactor, router: router)
+        
+        presenter.vc = vc
+        router.vc = vc
+        
+        return vc
+    }
+}
